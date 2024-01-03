9f544a036a2ab0e63feaf2002b9d625a2a953a6e
joelsartori <joelnicolass@gmail.com>
2024-01-03 17:15:23 -0300

    [docs] - add docs to entity rate

--- src/features/calculator/domain/entities/rate/rate.entity.ts
+++ src/features/calculator/domain/entities/rate/rate.entity.ts
@@ -1,14 +1,21 @@
 import { Currency } from "../currency/currency.entity";
 
+/**
+ * Represents a rate entity that contains the base currency and exchange rates.
+ */
 export class Rate {
   private _baseCurrency: Currency;
   private _rates: Record<Currency["id"], number>[];
 
+  /**
+   * Creates a new Rate instance.
+   * @param baseCurrency The base currency.
+   * @param rates The exchange rates.
+   */
   constructor(baseCurrency: Currency, rates: Record<Currency["id"], number>[]) {
     this._baseCurrency = baseCurrency;
     this._rates = rates;
   }
-
   get baseCurrency(): Currency {
     return this._baseCurrency;
   }
