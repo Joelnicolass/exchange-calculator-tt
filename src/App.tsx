6b7e2f64c81b3bcd0563ad09a38c51123ba46cf1
joelsartori <joelnicolass@gmail.com>
2024-01-07 11:41:58 -0300

    [fix] - delete unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import RootProvider from "./common/presentation/providers/root_provider/root_provider";
 import CalculatorScreen from "./features/exchange_calculator/presentation/views/calculator_screen/calculator_screen";
 import AppLayout from "./common/presentation/components/app_layout/app_layout";
